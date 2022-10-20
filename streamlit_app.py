efa629f11f251f4c189d9f41b55239cb4609120f
teamrelax <116233645+teamrelax@users.noreply.github.com>
2022-10-20 08:54:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
-test
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
